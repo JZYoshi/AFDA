5bd5b0c1f7c1e2a4a56b436c54a6926ef6f42389
zjiang <jzydsrs617@hotmail.com>
2021-03-14 10:13:15 +0100

    parallelize the computing of flight descriptors

--- flight phase on dataset/flight_descriptors.py
+++ flight phase on dataset/flight_descriptors.py
@@ -3,14 +3,15 @@
 
 
 ## importations
+import queue
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import json
-import os
 from os import listdir
 from db import get_db, init_db
 import time as clock
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import math
 
 ## functions
 def calculate_descriptor(phase):
@@ -68,6 +69,85 @@ def calculate_metar(lat,lon,time):
     except (FileNotFoundError, KeyError) as e:
         return [None,np.NaN,np.NaN,np.NaN]
 
+# compute descriptors and save them in database for each flight phase 
+def compute_descriptors_wrap_function(file_name, sql_query_queue):
+    df = pd.read_csv(path_to_dataset+file_name)
+
+    descent = df[df['phase']=='DE']
+    climb = df[df['phase']=='CL']
+    cruise = df[df['phase']=='CR']
+
+    # compute descriptors for climb phase
+    if not(climb.empty):
+        desc_climb = calculate_descriptor(climb)
+        data_takeof = climb.iloc[0]
+        lat = data_takeof['lat']
+        lon = data_takeof['lon']
+        time = (int(data_takeof['time'])//3600)*3600
+        desc_climb += calculate_metar(lat,lon,time)
+        sql_query_queue.put(["INSERT INTO climb (duration,avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,airport,temp_c,dewpoint_c,wind_spind_kt) \
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            desc_climb])
+    else:
+        sql_query_queue.put(["INSERT INTO climb DEFAULT VALUES"])
+
+    #compute descriptors for cruise phase
+    if not(cruise.empty):
+        desc_cruise = calculate_descriptor(cruise)
+        desc_cruise.append(cruise['baroaltitude'].mean())
+        desc_cruise.append(cruise['baroaltitude'].std())
+        sql_query_queue.put(["INSERT INTO cruise (duration,avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,mean_altitude,std_altitude) \
+            VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+            desc_cruise])
+    else:
+        sql_query_queue.put(['INSERT INTO cruise DEFAULT VALUES'])
+
+    # compute descriptors for descent phase
+    if not(descent.empty):
+        desc_descent = calculate_descriptor(descent)
+        data_landing = descent.iloc[-1]
+        lat = data_landing['lat']
+        lon = data_landing['lon']
+        time = (int(data_landing['time'])//3600)*3600
+        desc_descent += calculate_metar(lat,lon,time)
+        sql_query_queue.put(["INSERT INTO descent (duration, avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,airport,temp_c,dewpoint_c,wind_spind_kt) \
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            desc_descent])
+    else:
+        sql_query_queue.put(['INSERT INTO descent DEFAULT VALUES'])
+
+    # get airline corresponding to the flight
+    id_airline=file_name.split('_')[2][0:3]
+    icao24 = file_name.split('_')[1]
+    if len(airline[airline['ICAO']==id_airline])>0:
+        airline_name = airline[airline['ICAO']==id_airline].iloc[0]['Airline']
+    else:
+        airline_name = None
+        unknown_airline.append(id_airline)
+    db.execute("INSERT INTO general_info (icao,icao_airline,airline) \
+        VALUES (?,?,?)",(icao24,id_airline,airline_name))
+    sql_query_queue.put(["INSERT INTO general_info (icao,icao_airline,airline) \
+        VALUES (?,?,?)",(icao24,id_airline,airline_name)])
+
+
+def sql_query_executor(sql_query_queue, time):
+    db_connection = get_db()
+    while True:
+        try:
+            time["end"] = clock.time()
+            query = sql_query_queue.get(block=True, timeout=5)
+        except queue.Empty:
+            print("no more queries to execute")
+            db_connection.close()
+            break
+        else:
+            if len(query) == 1:
+                db_connection.execute(query[0])
+            else:
+                db_connection.execute(query[0], query[1])
+            db_connection.commit()
+    
+
 ## main
 if __name__=="__main__":
     # initialisation ( initialiase database, useful dataframe, print info)
@@ -81,82 +161,22 @@ if __name__=="__main__":
     airports=pd.read_csv('../data/airports.csv',
                     usecols=['Name','ICAO'])
 
-
     N = len(list_file_name)
     print('number of file to be processed:', N)
-    start = clock.time()
-    i=0
     unknown_airline=[]
+    time = { "start": clock.time(), "end": clock.time() }
 
-    # loop which compute descriptors and save them in database for each flight phase  
-    for file_name in list_file_name:
+    sql_query_queue = queue.SimpleQueue()
+    query_executor_thread = threading.Thread(target=sql_query_executor, args=[sql_query_queue, time])
+    query_executor_thread.start()
 
-        df = pd.read_csv(path_to_dataset+file_name)
+    with ThreadPoolExecutor() as executor:
+        for file_name in list_file_name:
+            executor.submit(compute_descriptors_wrap_function, file_name, sql_query_queue)
 
-        descent = df[df['phase']=='DE']
-        climb = df[df['phase']=='CL']
-        cruise = df[df['phase']=='CR']
-
-        db = get_db()
-
-        # compute descriptors for climb phase
-        if not(climb.empty):
-            desc_climb = calculate_descriptor(climb)
-            data_takeof = climb.iloc[0]
-            lat = data_takeof['lat']
-            lon = data_takeof['lon']
-            time = (int(data_takeof['time'])//3600)*3600
-            desc_climb += calculate_metar(lat,lon,time)
-            db.execute("INSERT INTO climb (duration,avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,airport,temp_c,dewpoint_c,wind_spind_kt) \
-                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                desc_climb )
-        else:
-            db.execute("INSERT INTO climb DEFAULT VALUES")
-
-        #compute descriptors for cruise phase
-        if not(cruise.empty):
-            desc_cruise = calculate_descriptor(cruise)
-            desc_cruise.append(cruise['baroaltitude'].mean())
-            desc_cruise.append(cruise['baroaltitude'].std())
-            db.execute("INSERT INTO cruise (duration,avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,mean_altitude,std_altitude) \
-                VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-                desc_cruise)
-        else:
-            db.execute('INSERT INTO cruise DEFAULT VALUES')
-
-        # compute descriptors for descent phase
-        if not(descent.empty):
-            desc_descent = calculate_descriptor(descent)
-            data_landing = descent.iloc[-1]
-            lat = data_landing['lat']
-            lon = data_landing['lon']
-            time = (int(data_landing['time'])//3600)*3600
-            desc_descent += calculate_metar(lat,lon,time)
-            db.execute("INSERT INTO descent (duration, avg_speed,std_speed,avg_vertrate_speed,std_vertrate_speed,max_spd,min_spd,max_vertrate_speed,min_vertrate_speed,airport,temp_c,dewpoint_c,wind_spind_kt) \
-                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                desc_descent )
-        else:
-            db.execute('INSERT INTO descent DEFAULT VALUES')
-
-        # get airline corresponding to the flight
-        id_airline=file_name.split('_')[2][0:3]
-        icao24 = file_name.split('_')[1]
-        if len(airline[airline['ICAO']==id_airline])>0:
-            airline_name = airline[airline['ICAO']==id_airline].iloc[0]['Airline']
-        else:
-            airline_name = None
-            unknown_airline.append(id_airline)
-        db.execute("INSERT INTO general_info (icao,icao_airline,airline) \
-            VALUES (?,?,?)",(icao24,id_airline,airline_name))
-
-        db.commit()
-        db.close()
-
-        # print info
-        i+=1
-        total_time = clock.time()-start
-        print('estimated time:', total_time/i*N)
-
+    query_executor_thread.join()
+    
+    total_time = time["end"] - time["start"]
     print("process end with success")
-    print('total_time:', total_time)
-    print('process time by file:', total_time/i)
+    print('total_time: {:.2f}s'.format(total_time))
+    print(f'process time by file: {math.ceil(total_time/len(list_file_name) * 1000)}ms')
